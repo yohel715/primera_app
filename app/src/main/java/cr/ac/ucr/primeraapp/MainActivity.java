2b8d83e3d37899ef5706ac8c660dd2f7cab39179
yohel715 <yohel715@gmail.com>
2020-08-21 19:30:12 -0600

    boton flotante  y lista funcional

--- app/src/main/java/cr/ac/ucr/primeraapp/MainActivity.java
+++ app/src/main/java/cr/ac/ucr/primeraapp/MainActivity.java
@@ -1,19 +1,33 @@
 package cr.ac.ucr.primeraapp;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+import java.util.ArrayList;
 
 import cr.ac.ucr.primeraapp.utils.AppPreferences;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+    private ArrayList<String> todosArr;
+    private ArrayAdapter<String> todosAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +36,16 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        //ListView <--> ArrayAdapter <--> ArrayList
+        ListView lvTodos = findViewById(R.id.lv_todos);
+        todosArr = new ArrayList<>();
+        todosAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1 , todosArr);
+
+        lvTodos.setAdapter(todosAdapter);
+
+        //todosArr.add("prueba");
+
     }
 
     @Override
@@ -47,5 +71,44 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
         finish();
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.fab_add_todo:
+                showAlert();
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void showAlert() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        //cargar el layout flotante
+        LayoutInflater inflater = getLayoutInflater();
+        //cargar la vista del layout
+        final View view =  inflater.inflate(R.layout.dialog_add_task, null);
+
+        builder.setView(view)
+                .setPositiveButton(R.string.add, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //capturar el tap
+                        TextInputEditText etTaskName = view.findViewById(R.id.et_task_name);
+
+                        String taskName = etTaskName.getText().toString();
+                        if(!taskName.isEmpty()){
+                            todosArr.add(taskName);
+                            todosAdapter.notifyDataSetChanged();
+                            dialog.dismiss();
+                        }
+                    }
+                })
+                //null cierra la alter
+                .setNegativeButton(R.string.cancel, null);
+        builder.create();
+        builder.show();
     }
 }
